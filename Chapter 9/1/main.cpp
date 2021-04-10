e8acdd94693dcf78952281e4a834b58dc8535d1d
Armen <armen.baburian@yandex.ru>
2021-04-10 07:21:45 +0300

    Hey

--- Chapter 9/1/main.cpp
+++ Chapter 9/1/main.cpp
@@ -1,15 +1,48 @@
 #include <iostream>
-#include <iomanip>
+#include <string>
 using namespace std;
-int main() {
-    int a;
-    cout << "Enter some number :";cin >> a;int k=1;
-    for (int i=0;i<20;i++){
-        for (int j=0;j<10;j++){
-            cout <<setw(5)<<k*a;
-            k++;
-        }
-        cout <<endl;
+class publ{
+protected:
+    string name;float cost;
+public:
+    publ(): name(""),cost(0){};
+    void getdata(){
+        cout <<"Enter name of publication (string): ";cin >>name;
+        cout <<"Enter cost of publication: ";cin >>cost;
     }
+    void putdata(){
+        cout <<"Name of publication: "<<name<<endl<<"Cost of publication: "<<cost<<endl;
+    }
+
+};
+class book : public publ{
+protected:
+    int page;
+public:
+    void getdata(){
+        publ::getdata();
+        cout <<"How much page in book: ";cin >> page;
+    }
+    void putdata(){
+        publ::putdata();
+        cout<<"Page in book: "<<page<<endl;
+    }
+};
+class type : public publ{
+protected:
+    float time;
+public:
+    void getdata(){
+        publ::getdata();
+        cout<<"How much time in audiobook: ";cin >>time;
+    }
+    void putdata(){
+        publ::putdata();
+        cout <<"Time of audiobook: "<<time<<endl;
+    }
+
+};
+int main() {
+    book a;type b;a.getdata();b.getdata();a.putdata();b.putdata();
     return 0;
 }
