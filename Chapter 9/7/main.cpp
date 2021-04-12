5dedddcea7e47c21aa19597cf3b0cb13b1332230
Armen <armen.baburian@yandex.ru>
2021-04-12 20:41:42 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Chapter 9/4/.idea/.gitignore

--- Chapter 9/7/main.cpp
+++ Chapter 9/7/main.cpp
@@ -21,6 +21,7 @@ public:
 class CountDn : public Counter
 {
 public:
+    using Counter::operator++;
     CountDn() : Counter() // конструктор без параметров
     { }
     CountDn(int c) : Counter(c) // конструктор с одним параметром
@@ -30,6 +31,10 @@ public:
 };
 class CountDn1 : public CountDn{
 public:
+    using Counter::operator++;
+    using CountDn::operator--;
+    CountDn1() : CountDn() // конструктор без параметров
+    { }
     CountDn1( int i): CountDn(i) {}
 
     CountDn1 operator--(int ){
@@ -42,16 +47,18 @@ public:
 ///////////////////////////////////////////////////////////
 int main()
 {
+    CountDn1 cd1;cd1++
+    ;++cd1;
     CountDn c1; // переменные класса CountDn
     CountDn c2(100);
     cout << "\nc1 = " << c1.get_count();// выводим значения на экран
     cout << "\nc2 = " << c2.get_count();
     ++c1; ++c1; ++c1; // увеличиваем c1
-    cout << "\nc1 = " << c1.get_count();// показываем результат
+    /*cout << "\nc1 = " << c1.get_count();// показываем результат
     --c2; --c2; // уменьшаем c2
     cout << "c2 = " << c2.get_count(); // показываем результат
     CountDn c3 = --c2; // создаем переменную c3 на основе c2
     cout << "\nc3 = " << c3.get_count();// показываем значение
-    cout << endl;
+    cout << endl;*/
     return 0;
 } 
