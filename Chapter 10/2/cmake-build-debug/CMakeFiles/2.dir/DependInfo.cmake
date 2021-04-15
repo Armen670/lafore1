9886dc11a3d6d75151665d5d69f60067fa7e1b04
Armen <armen.baburian@yandex.ru>
2021-04-15 21:37:14 +0300

    Merge remote-tracking branch 'origin/master'

--- Chapter 10/2/cmake-build-debug/CMakeFiles/2.dir/DependInfo.cmake
+++ Chapter 10/2/cmake-build-debug/CMakeFiles/2.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/armen/CLionProjects/Lafore/Chapter 3/2/main.cpp" "C:/Users/armen/CLionProjects/Lafore/Chapter 3/2/cmake-build-debug/CMakeFiles/2.dir/main.cpp.obj"
+  "C:/Users/armen/CLionProjects/Lafore/Chapter 10/2/main.cpp" "C:/Users/armen/CLionProjects/Lafore/Chapter 10/2/cmake-build-debug/CMakeFiles/2.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- Chapter 10/2/main.cpp
+++ Chapter 10/2/main.cpp
@@ -1,18 +1,40 @@
+// newstr.cpp
+// использование оператора new для выделения памяти под строку
 #include <iostream>
+#include <cstring>
+#include "Cctype"
+// для strcpy(), и т.д.
 using namespace std;
-int main() {
-    cout << "1.Celsius to Fahrenheit"<<endl<<
-            "2.Fahrenheit to Celsius";
-    int a;int b;cin >> a;double c = 0.55555556;
-    switch (a){
-        case 1:
-            cout << "Enter Celsius:";cin>>b;
-            cout << "Fahrenheit:"<<(b*(1/c))+32;
-            break;
-        case 2:
-            cout <<"Enter Fahrenheit:";cin >>b;
-            cout <<"Ceelsius :"<<(b-32)*c;
-            break;
+class String
+{
+private:
+    char* str; // указатель на строку
+public:
+    String(char* s) // конструктор с одним параметром
+    {
+        int length = strlen(s); // вычисляем длину строки
+        str = new char[length + 1]; // выделяем необходимую память
+        strcpy(str, s); // копируем строку
     }
+    ~String() // деструктор
+    {
+        cout << "Delete string\n";
+        delete[] str; // освобождаем память
+    }
+    void display() // покажем строку на экране
+    {
+        cout << str << endl;
+    }
+    void upit(){
+        for (int i=0;i<strlen(str);i++){
+            *(str+i)=toupper(*(str+i));
+        }
+    }
+};
+int main()
+{
+    String s1 = "What have you been up to?";
+    cout << "s1 = ";
+    s1.display();s1.upit();s1.display();
     return 0;
 }
